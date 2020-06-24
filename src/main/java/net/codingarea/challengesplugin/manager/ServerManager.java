35ee4d4f61a8e4ae91e209fbf0fb437a2a930565
anweisen <60273047+anweisen@users.noreply.github.com>
2020-06-24 22:26:39 +0200

    Dropbug fixed

--- src/main/java/net/codingarea/challengesplugin/manager/ServerManager.java
+++ src/main/java/net/codingarea/challengesplugin/manager/ServerManager.java
@@ -6,13 +6,15 @@ import net.codingarea.challengesplugin.manager.lang.Translation;
 import net.codingarea.challengesplugin.timer.ChallengeTimer;
 import net.codingarea.challengesplugin.utils.AnimationUtil.AnimationSound;
 import net.codingarea.challengesplugin.utils.Utils;
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -57,7 +59,6 @@ public class ServerManager {
 		boolean setAllToSpectator = true;
 
 		if (cause == ChallengeEndCause.PLAYER_CHALLENGE_GOAL_REACHED || cause == ChallengeEndCause.LAST_MAN_STANDING) {
-			setAllToSpectator = false;
 			Bukkit.broadcastMessage(Translation.CHALLENGE_END_GOAL_REACHED.get().replace("%time%", ChallengeTimer.getTimeDisplay(plugin.getChallengeTimer().getSeconds())).replace("%winner%", players).replace("%seed%", Bukkit.getWorlds().get(0).getSeed() + ""));
 		} else if (cause == ChallengeEndCause.TIMER_END) {
 			Bukkit.broadcastMessage(Translation.CHALLENGE_END_TIMER_END.get().replace("%time%", ChallengeTimer.getTimeDisplay(plugin.getChallengeTimer().getSeconds())).replace("%winner%", players).replace("%seed%", Bukkit.getWorlds().get(0).getSeed() + ""));
@@ -69,11 +70,29 @@ public class ServerManager {
 
 		if (setAllToSpectator) {
 			for (Player currentPlayer : Bukkit.getOnlinePlayers()) {
+				dropInventory(currentPlayer.getInventory(), currentPlayer.getLocation());
 				currentPlayer.setGameMode(GameMode.SPECTATOR);
+				currentPlayer.updateInventory();
 			}
 		}
 
 		playEndEffect();
+
+	}
+
+	private void dropInventory(PlayerInventory inventory, Location location) {
+
+		List<ItemStack> items = new ArrayList<>();
+		items.addAll(Arrays.asList(inventory.getContents()));
+		items.addAll(Arrays.asList(inventory.getArmorContents()));
+
+		inventory.clear();
+
+		for (ItemStack currentItemStack : items) {
+			if (currentItemStack.getType() == Material.AIR) continue;
+			Item currentItem = (Item) location.getWorld().spawnEntity(location, EntityType.DROPPED_ITEM);
+			currentItem.setItemStack(currentItemStack);
+		}
 
 	}
 
