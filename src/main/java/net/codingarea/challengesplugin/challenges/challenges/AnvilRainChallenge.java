4b01f0cb9a773895a2593ec37db9c406d2efe8d8
anweisen <60273047+anweisen@users.noreply.github.com>
2020-06-27 21:28:37 +0200

    spawn algorithm changed

--- src/main/java/net/codingarea/challengesplugin/challenges/challenges/AnvilRainChallenge.java
+++ src/main/java/net/codingarea/challengesplugin/challenges/challenges/AnvilRainChallenge.java
@@ -8,10 +8,10 @@ import net.codingarea.challengesplugin.manager.lang.ItemTranslation;
 import net.codingarea.challengesplugin.manager.menu.MenuType;
 import net.codingarea.challengesplugin.utils.ItemBuilder;
 import net.codingarea.challengesplugin.utils.Utils;
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -19,7 +19,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author anweisen & Dominik
@@ -31,23 +35,70 @@ import java.util.Random;
 public class AnvilRainChallenge extends AdvancedChallenge implements ITimerStatusExecutor, Listener {
 
 	private final Random random;
+	private final BossBar bossBar;
+	private int average = 0;
 
 	public AnvilRainChallenge() {
 		this.random = new Random();
 		this.menu = MenuType.CHALLENGES;
-		this.maxValue = 60;
+		this.maxValue = 20;
+		bossBar = Bukkit.createBossBar("AnvilRain", BarColor.BLUE, BarStyle.SOLID);
+		bossBar.setVisible(false);
 	}
 
 	@Override
-	public void onValueChange(ChallengeEditEvent event) { }
+	public String getChallengeName() {
+		return "anvilrain";
+	}
+
+	private void updateBossBar() {
+
+		Utils.forEachPlayerOnline(bossBar::addPlayer);
+
+		if (!enabled) {
+			bossBar.setVisible(false);
+			return;
+		}
+
+		bossBar.setVisible(true);
+		bossBar.setProgress(1);
+
+		String title;
+
+		if (!Challenges.timerIsStarted()) {
+			title = "§cTimer is paused";
+			bossBar.setColor(BarColor.RED);
+		} else {
+			title = "§7Anvils per chunk §9" + value + " §8● §7Anvils/s §9" + average;
+			bossBar.setColor(BarColor.BLUE);
+		}
+
+		bossBar.setTitle(title);
+
+
+	}
+
+	@Override
+	public void onValueChange(ChallengeEditEvent event) {
+		updateBossBar();
+	}
 
 	@Override
 	public void onEnable(ChallengeEditEvent event) {
 		this.nextActionInSeconds = 1;
+		updateBossBar();
 	}
 
 	@Override
-	public void onDisable(ChallengeEditEvent event) { }
+	public void onDisable(ChallengeEditEvent event) {
+		for (World currentWorld : Bukkit.getWorlds()) {
+			for (FallingBlock currentFallingBlock : currentWorld.getEntitiesByClass(FallingBlock.class)) {
+				if (!currentFallingBlock.getBlockData().getMaterial().name().toLowerCase().contains("anvil")) continue;
+				currentFallingBlock.remove();
+			}
+		}
+		updateBossBar();
+	}
 
 	@Override
 	public void onTimerStart() {
@@ -58,80 +109,57 @@ public class AnvilRainChallenge extends AdvancedChallenge implements ITimerStatu
 	public void onTimeActivation() {
 
 		this.nextActionInSeconds = 1;
-		for (Player currentPlayer : Bukkit.getOnlinePlayers()) {
-			if (currentPlayer.getGameMode() == GameMode.SPECTATOR) continue;
-			Location anvil = currentPlayer.getLocation().getBlock().getLocation().clone().add(0.5, 0, 0.5);
-			anvil.setY(calculateHeight(anvil));
-			spawnAnvils(anvil, calculateRadius(), random);
-		}
+		updateBossBar();
+
+		Bukkit.getScheduler().runTask(Challenges.getInstance(), () -> {
+
+			List<Chunk> chunks = new ArrayList<>();
+			int height = 0;
+
+			for (Player currentPlayer : Bukkit.getOnlinePlayers()) {
+				if (currentPlayer.getGameMode() == GameMode.SPECTATOR) continue;
+
+				if ((currentPlayer.getLocation().getBlockY() + 20) > height) height = currentPlayer.getLocation().getBlockY() + 25;
+
+				Chunk chunk = currentPlayer.getLocation().getChunk();
+				World world = chunk.getWorld();
+
+				for (int i = -1; i <= 1; i++) {
+					for (int j = -1; j <= 1; j++) {
+						Chunk currentChunk = world.getChunkAt(chunk.getX() + i, chunk.getZ() + j);
+						if (chunks.contains(currentChunk)) continue;
+						chunks.add(currentChunk);
+					}
+				}
+			}
+
+			int spawned = 0;
+			for (Chunk currentChunk : chunks) {
+				spawnAnvils(currentChunk, height);
+				spawned += value;
+			}
+			average = spawned;
+
+		});
 
 	}
 
-	private int calculateHeight(Location player) {
-
-		int i = 0;
-		while (player.getBlock().isPassable() && player.getBlockY() < 250 && i < 35) {
-			player.add(0, 1, 0);
-			i++;
-		}
-
-		if (i < 15) {
-			int j = Utils.getHighestBlock(player).getBlockY();
-			return (j - 15) > player.getBlockY() ? j : player.getBlockY() + 10;
-		}
-
-		return i - 3 + player.getBlockY();
-
-	}
-
-	private int calculateRadius() {
-		if (value <= 10) {
-			return 10;
-		} else if (value <= 30) {
-			return 25;
-		} else if (value <= 45) {
-			return 40;
-		} else {
-			return 50;
-		}
-	}
-
-	private void spawnAnvils(Location middle, int radius, Random random) {
-
+	private void spawnAnvils(Chunk chunk, int height) {
 		for (int i = 0; i < value; i++) {
-			spawnAnvil(getRandomLocationInRadius(middle, radius, random).add(0, 1, 0)).getLocation().getBlockY();
+			Location blockLocation = getRandomLocationInChunk(chunk);
+			blockLocation.setY(height);
+			spawnAnvil(blockLocation.add(0, 1, 0));
 		}
-
 	}
 
-	private Location getRandomLocationInRadius(Location middle, int radius, Random random) {
-		int[] distance = getRandomDistance(radius, random);
-		return middle.clone().add(distance[0], 0, distance[1]);
+	private Location getRandomLocationInChunk(Chunk chunk) {
+		int x = getRandomIntInRadius(16);
+		int z = getRandomIntInRadius(16);
+		return chunk.getBlock(x, 0, z).getLocation().add(0.5, 0, 0.5);
 	}
 
-	private int getRandomIntInRadius(int radius, Random random) {
-		return random.nextInt(radius) - radius / 2;
-	}
-
-	private int[] getRandomDistance(int radius, Random random) {
-
-		int[] array = new int[2];
-
-		int a = getRandomIntInRadius(radius, random);
-		int b = getRandomIntInRadius(radius, random);
-
-		if (a > b) {
-			a += random.nextInt(4);
-			b -= random.nextInt(4);
-		} else {
-			a -= random.nextInt(4);
-			b += random.nextInt(4);
-		}
-		array[0] = a;
-		array[1] = b;
-
-		return array;
-
+	private int getRandomIntInRadius(int radius) {
+		return random.nextInt(radius);
 	}
 
 	private FallingBlock spawnAnvil(Location anvil) {
@@ -148,7 +176,14 @@ public class AnvilRainChallenge extends AdvancedChallenge implements ITimerStatu
 
 		if (!enabled || !Challenges.timerIsStarted()) return;
 		if (!(event.getEntity() instanceof FallingBlock)) return;
+		if (!((FallingBlock)event.getEntity()).getBlockData().getMaterial().name().toLowerCase().contains("anvil")) return;
 		if (event.getBlock().getLocation().clone().add(0, -1, 0).getBlock().getType() == Material.AIR) return;
+		Location location = event.getBlock().getLocation().add(0,-1, 0);
+
+		if (location.getBlock().getType() == Material.GRASS_PATH || Arrays.asList(Utils.getFlowers()).contains(location.getBlock().getType())) {
+			location.getBlock().setType(Material.AIR, true);
+		}
+
 		event.getBlock().setType(Material.AIR);
 		event.setCancelled(true);
 		event.getEntity().remove();
