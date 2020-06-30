e8d69b4d151c4290b01f347eca27974eaace634e
anweisen <60273047+anweisen@users.noreply.github.com>
2020-06-30 12:25:05 +0200

    code cleanup

--- src/main/java/net/codingarea/challengesplugin/commands/TimerCommand.java
+++ src/main/java/net/codingarea/challengesplugin/commands/TimerCommand.java
@@ -6,12 +6,14 @@ import net.codingarea.challengesplugin.manager.lang.Translation;
 import net.codingarea.challengesplugin.timer.ChallengeTimer;
 import net.codingarea.challengesplugin.timer.ChallengeTimer.TimerMode;
 import net.codingarea.challengesplugin.utils.AnimationUtil.AnimationSound;
+import net.codingarea.challengesplugin.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,14 +29,14 @@ import java.util.List;
 
 public class TimerCommand implements CommandExecutor, TabCompleter {
 
-	private Challenges plugin;
+	private final Challenges plugin;
 
 	public TimerCommand(Challenges plugin) {
 		this.plugin = plugin;
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
 
 		if (!(sender instanceof Player)) return true;
 
@@ -43,22 +45,21 @@ public class TimerCommand implements CommandExecutor, TabCompleter {
 		if (command.getName().equals("start")) {
 			if (!plugin.getChallengeTimer().isPaused()) {
 				player.sendMessage(plugin.getStringManager().TIMER_PREFIX + Translation.TIMER_ALREADY_STARTED.get());
-				return true;
+			} else {
+				plugin.getChallengeTimer().resume(player);
 			}
-
-			plugin.getChallengeTimer().resume(player);
 			return true;
 		} else if (command.getName().equals("pause")) {
 			plugin.getChallengeTimer().stopTimer(player, true);
 			return true;
 		}
 
+		// command can only be the "/timer" command here
 		if (args.length == 0) {
 			plugin.getChallengeTimer().getMenu().openMainMenu(player);
 			AnimationSound.OPEN_SOUND.play(player);
 			return true;
 		}
-
 
 		switch (args[0].toLowerCase()) {
 
@@ -88,39 +89,15 @@ public class TimerCommand implements CommandExecutor, TabCompleter {
 					return true;
 				}
 
-				int seconds = 0;
+				int seconds = calculateSeconds(args);
 
-				for (String currentArg : args) {
-
-					int multiplier = getMultiplierByEnding(currentArg.substring(currentArg.length() - 1));
-
-					String raw;
-					if (multiplier != -1) {
-						raw = currentArg.substring(0, currentArg.length() - 1);
-					} else {
-						raw = currentArg;
-						multiplier = 1;
-					}
-
-					try {
-						seconds += Integer.parseInt(raw) * multiplier;
-					} catch (NumberFormatException ignored) { }
-
-				}
-
-				if (seconds > ChallengeTimer.MAX_VALUE + 1) {
+				if (seconds >= ChallengeTimer.MAX_VALUE) {
 					seconds = ChallengeTimer.MAX_VALUE + 1;
-				}
-				if (seconds < ChallengeTimer.MIN_VALUE) {
+				} else if (seconds < ChallengeTimer.MIN_VALUE) {
 					seconds = ChallengeTimer.MIN_VALUE;
 				}
 
-				if (plugin.getChallengeTimer().getMode() == TimerMode.UP) {
-					plugin.getChallengeTimer().setSeconds(seconds);
-				} else {
-					plugin.getChallengeTimer().setMaxSeconds(seconds);
-				}
-
+				plugin.getChallengeTimer().setMaxSeconds(seconds);
 				Bukkit.broadcastMessage(plugin.getStringManager().TIMER_PREFIX + Translation.TIMER_SET_TIME.get().replace("%time%", ChallengeTimer.getTimeDisplay(seconds)).replace("%player%", player.getName()));
 				break;
 
@@ -175,70 +152,65 @@ public class TimerCommand implements CommandExecutor, TabCompleter {
 
 	}
 
-	@Override
-	public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+	private int calculateSeconds(String[] args) {
 
-		List<String> list = new ArrayList<>();
+		int seconds = 0;
 
-		if (command != null && command.getName().equals("timer")) {
-			if (args.length == 1) {
-				list.addAll(Arrays.asList("show", "hide", "mode", "set", "pause", "resume",  "reset", "start"));
+		for (String currentArg : args) {
 
-				if (!args[0].isEmpty()) {
-					List<String> remove = new ArrayList<>();
+			int multiplier = getMultiplierByEnding(currentArg.substring(currentArg.length() - 1));
 
-					for (String currentString : list) {
-						if (!currentString.startsWith(args[0].toLowerCase())) {
-							remove.add(currentString);
-						}
-					}
-					list.removeAll(remove);
-				}
-				return list;
-			} else if (args.length >= 2) {
-
-				if (args[0].equalsIgnoreCase("set")) {
-
-					if (args[args.length - 1].isEmpty()) return list;
-
-					list.addAll(Arrays.asList("d", "h", "m", "s"));
-					Collections.sort(list);
-
-					if (args[args.length - 1].isEmpty()) {
-						return null;
-					}
-
-					for (String currentEnding : list) {
-						if (args[args.length - 1].toLowerCase().endsWith(currentEnding)) {
-							return null;
-						}
-					}
-
-					return list;
-
-				} else if (args[0].equalsIgnoreCase("mode")) {
-					list.addAll(Arrays.asList("up", "down"));
-
-					if (!args[1].isEmpty()) {
-
-						List<String> remove = new ArrayList<>();
-
-						for (String currentString : list) {
-							if (!currentString.startsWith(args[0].toLowerCase())) {
-								remove.add(currentString);
-							}
-						}
-						list.removeAll(remove);
-					}
-
-				}
-
-
+			String raw;
+			if (multiplier != -1) {
+				raw = currentArg.substring(0, currentArg.length() - 1);
+			} else {
+				raw = currentArg;
+				multiplier = 1;
 			}
+
+			try {
+				seconds += Integer.parseInt(raw) * multiplier;
+			} catch (NumberFormatException ignored) { }
 
 		}
 
-		return list;
+		return seconds;
 
 	}
+
+	@Override
+	public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+
+		if (!command.getName().equals("timer")) return null;
+
+		if (args.length == 1) {
+			return Utils.getMatchingSuggestions(args[0].toLowerCase(), "show", "hide", "mode", "set", "pause", "resume", "reset", "start");
+		} else if (args.length >= 2) {
+
+			if (args[0].equalsIgnoreCase("set")) {
+
+				if (args[args.length - 1].isEmpty()) return null;
+
+				List<String> list = new ArrayList<>(Arrays.asList("d", "h", "m", "s"));
+				Collections.sort(list);
+
+				if (args[args.length - 1].isEmpty()) return null;
+
+				for (String currentEnding : list) {
+					if (args[args.length - 1].toLowerCase().endsWith(currentEnding)) {
+						return null;
+					}
+				}
+
+				return list;
+
+			} else if (args[0].equalsIgnoreCase("mode")) {
+				return Utils.getMatchingSuggestions(args[1].toLowerCase(), "up", "down");
+			}
+		}
+
+		return null;
+
+	}
+
 }
