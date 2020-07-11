68efc38f67d4b502b6d6877f338664d5043a8919
anweisen <60273047+anweisen@users.noreply.github.com>
2020-07-11 13:05:47 +0200

    clearConfig method & backpackConfig removed

--- src/main/java/net/codingarea/challengesplugin/manager/ConfigManager.java
+++ src/main/java/net/codingarea/challengesplugin/manager/ConfigManager.java
@@ -12,50 +12,28 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 public class ConfigManager {
 
-	private YamlConfig backpackConfig;
+	private YamlConfig internalConfig;
 	private YamlConfig positionConfig;
-	private YamlConfig internalConfig; // This is the only config wich wont be reset in this manager
-
-	public YamlConfig getBackpackConfig() {
-		return backpackConfig;
-	}
 
 	public YamlConfig getInternalConfig() {
 		return internalConfig;
-	}
-
-	public YamlConfig getPositionConfig() {
-		return positionConfig;
-	}
-
-	public void setBackpackConfig(YamlConfig backpackConfig) {
-		this.backpackConfig = backpackConfig;
 	}
 
 	public void setInternalConfig(YamlConfig internalConfig) {
 		this.internalConfig = internalConfig;
 	}
 
+	public YamlConfig getPositionConfig() {
+		return positionConfig;
+	}
+
 	public void setPositionConfig(YamlConfig positionConfig) {
 		this.positionConfig = positionConfig;
 	}
 
-	public void clearConfig(YamlConfig toClear) {
-
-		FileConfiguration config = toClear.toFileConfig();
-
-		for (String currentKey : config.getKeys(false)) {
-			config.set(currentKey, null);
-		}
-
-		toClear.save();
-	}
-
-	public void clearSession() {
-		clearConfig(backpackConfig);
-		clearConfig(positionConfig);
-		internalConfig.toFileConfig().set("ingame", false);
-		internalConfig.save();
+	public void reset() {
+		positionConfig.clear(true);
+		internalConfig.clear(true, "level-name", "reset");
 	}
 
 }
