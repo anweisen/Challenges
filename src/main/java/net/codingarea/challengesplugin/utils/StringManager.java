f1ec45667dfcddad470927a1bf148e3c5ab97449
anweisen <60273047+anweisen@users.noreply.github.com>
2020-07-18 11:13:00 +0200

    Loading prefix from config

--- src/main/java/net/codingarea/challengesplugin/utils/StringManager.java
+++ src/main/java/net/codingarea/challengesplugin/utils/StringManager.java
@@ -1,5 +1,6 @@
 package net.codingarea.challengesplugin.utils;
 
+import net.codingarea.challengesplugin.utils.commons.Log;
 import org.bukkit.configuration.file.FileConfiguration;
 
 /**
@@ -18,16 +19,17 @@ public class StringManager {
     public String POSITION_PREFIX = PREFIX_TEMPLATE.replace("%name%", "§9Position");
     public String BACKPACK_PREFIX = PREFIX_TEMPLATE.replace("%name%", "§aBackpack");
     public String DAMAGE_PREFIX = PREFIX_TEMPLATE.replace("%name%", "§cDamage");
+    public String BINGO_PREFIX = PREFIX_TEMPLATE.replace("%name%", "§dBingo");
 
-    public String MENU_TITLE = "Menu";
-    public String MENU_TITLE_END = " ● %page";
-    public String CHALLENGES_TITLE = MENU_TITLE + " ➜ Challenges";
-    public String SETTINGS_TITLE = MENU_TITLE + " ➜ Settings";
-    public String DIFFICULTY_TITLE = MENU_TITLE +  " ➜ Difficulty";
-    public String BLOCKS_TITLE = MENU_TITLE +  " ➜ Items and Blocks";
-    public String GOALS_TITLE = MENU_TITLE +  " ➜ Game-Goal";
-    public String TIMER_TITLE = MENU_TITLE +  " ➜ Timer";
-    public String DAMAGE_TITLE = MENU_TITLE +  " ➜ Damage";
+    public String MENU_TITLE = "§8» §9Menu";
+    public String MENU_TITLE_END = " §8┃ §9%page";
+    public String CHALLENGES_TITLE = MENU_TITLE + " §8• §9Challenges";
+    public String SETTINGS_TITLE = MENU_TITLE + " §8• §9Settings";
+    public String DIFFICULTY_TITLE = MENU_TITLE +  " §8• §9Difficulty";
+    public String BLOCKS_TITLE = MENU_TITLE +  " §8• §9Items and Blocks";
+    public String GOALS_TITLE = MENU_TITLE +  " §8• §9Game-Goal";
+    public String TIMER_TITLE = MENU_TITLE +  " §8• §9Timer";
+    public String DAMAGE_TITLE = MENU_TITLE +  " §8• §9Damage";
 
     public String ACTIONBAR_TIMER_STOP = "§8▶ §7Time: §cstopped §8◀";
     public String ACTIONBAR_TIMER_TIME_UP = "§8▶ §7Time: §b%time% §8◀";
@@ -37,8 +39,6 @@ public class StringManager {
 
     public void load(FileConfiguration config) {
 
-        if (config == null) throw new NullPointerException("FileConfig cannot be null!");
-
         ACTIONBAR_TIMER_STOP = config.getString("timer-stopped");
         ACTIONBAR_TIMER_TIME_UP = config.getString("timer-up");
         ACTIONBAR_TIMER_TIME_DOWN = config.getString("timer-down");
@@ -47,6 +47,15 @@ public class StringManager {
         ACTIONBAR_TIMER_TIME_UP_INFO = config.getString("timer-up-info");
 
         PREFIX_TEMPLATE = config.getString("prefix");
+        if (PREFIX_TEMPLATE == null) Log.severe("Could not load prefix :: Prefix cannot be null");
+
+        CHALLENGE_PREFIX = PREFIX_TEMPLATE.replace("%name%", "§6Challenges");
+        TIMER_PREFIX = PREFIX_TEMPLATE.replace("%name%", "§5Timer");
+        MASTER_PREFIX = PREFIX_TEMPLATE.replace("%name%", "§eMaster");
+        POSITION_PREFIX = PREFIX_TEMPLATE.replace("%name%", "§9Position");
+        BACKPACK_PREFIX = PREFIX_TEMPLATE.replace("%name%", "§aBackpack");
+        DAMAGE_PREFIX = PREFIX_TEMPLATE.replace("%name%", "§cDamage");
+        BINGO_PREFIX = PREFIX_TEMPLATE.replace("%name%", "§dBingo");
 
     }
 
