8a0c8bf1059eb8e02682a9c902b2bc1e1274023e
anweisen <60273047+anweisen@users.noreply.github.com>
2020-08-11 18:59:15 +0200

    Positionlist added

--- src/main/java/net/codingarea/challengesplugin/commands/PositionCommand.java
+++ src/main/java/net/codingarea/challengesplugin/commands/PositionCommand.java
@@ -2,9 +2,11 @@ package net.codingarea.challengesplugin.commands;
 
 import net.codingarea.challengesplugin.Challenges;
 import net.codingarea.challengesplugin.manager.lang.LanguageManager;
+import net.codingarea.challengesplugin.manager.lang.Prefix;
 import net.codingarea.challengesplugin.manager.lang.Translation;
 import net.codingarea.challengesplugin.utils.PositionUtil;
 import net.codingarea.challengesplugin.utils.PositionUtil.ChallengePosition;
+import net.codingarea.challengesplugin.utils.Utils;
 import net.codingarea.challengesplugin.utils.YamlConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -13,6 +15,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,22 +29,21 @@ import java.util.List;
 
 public class PositionCommand implements CommandExecutor, TabCompleter {
 
-	private final List<ChallengePosition> positions;
+	private final List<ChallengePosition> positions = new ArrayList<>();
 
 	public PositionCommand() {
-		positions = new ArrayList<>();
 		loadPositions();
 	}
 
 	public void loadPositions() {
 
-		ConfigurationSection section = getConfig().getConfig().getConfigurationSection("position");
+		ConfigurationSection section = getConfig().toFileConfig().getConfigurationSection("position");
 
 		if (section == null) return;
 
 		for (String currentPositionKey : section.getKeys(false)) {
 
-			ChallengePosition currentPosition = PositionUtil.loadPosition(getConfig().getConfig(), currentPositionKey);
+			ChallengePosition currentPosition = PositionUtil.loadPosition(getConfig().toFileConfig(), currentPositionKey);
 			if (currentPosition == null) continue;
 			positions.add(currentPosition);
 
@@ -50,98 +52,91 @@ public class PositionCommand implements CommandExecutor, TabCompleter {
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
 
 		if (!(sender instanceof Player)) return true;
-
-		if (args.length != 1) {
-			sender.sendMessage(Challenges.getInstance().getStringManager().POSITION_PREFIX + LanguageManager.syntax("/position <Name>"));
-			return true;
-		}
-
 		Player player = (Player) sender;
 
-		String name = args[0].toLowerCase();
+		if (args.length == 0) {
 
-		if (getLocationNamesLowerCase().contains(name)) {
+			if (positions.isEmpty()) {
+				sender.sendMessage(Prefix.POSITIONS + Translation.POSITION_EMPTY.get());
+			} else {
 
-			/*
-			 * This will be executed when a position was found by the given name
-			 */
+				sender.sendMessage(Prefix.POSITIONS + Translation.POSITION_LIST.get());
+				for (ChallengePosition currentPosition : positions) {
+					sender.sendMessage(Prefix.POSITIONS + Translation.POSITION_GET.get().replace("%name%", currentPosition.getName()).replace("%position%", currentPosition.toString() + " §eDistance: §7" + (int)currentPosition.getLocation().distance(player.getLocation())));
+				}
 
-			ChallengePosition position = getPositionByLowerCaseName(name);
-			sender.sendMessage(Challenges.getInstance().getStringManager().POSITION_PREFIX + Translation.POSITION_GET.get()
-					.replace("%name%", position.getName()).replace("%position%", position.toString() + " §eDistance: §7" + (int)position.getLocation().distance(player.getLocation()) + ""));
-
-		} else {
-
-			String displayName = args[0];
-
-			ChallengePosition position = new ChallengePosition(player.getLocation(), displayName, player.getName());
-			positions.add(position);
-
-			PositionUtil.savePosition(getConfig().getConfig(), position);
-			getConfig().save();
-
-			for (Player currentPlayer : Bukkit.getOnlinePlayers()) {
-				String positionName = "§e" + displayName + " §8(" + position.toString() + "§8)";
-				currentPlayer.sendMessage(Challenges.getInstance().getStringManager().POSITION_PREFIX + Translation.POSITION_CREATE.get().replace("%player%", player.getName()).replace("%position%", positionName));
 			}
 
+			sender.sendMessage(Prefix.POSITIONS + Translation.POSITION_CREATE.get());
+
+		} else if (args.length == 1) {
+
+			String name = args[0].toLowerCase();
+
+			ChallengePosition position = getPositionByName(name);
+			if (position != null) {
+
+				sender.sendMessage(Prefix.POSITIONS + Translation.POSITION_GET.get().replace("%name%", position.getName()).replace("%position%", position.toString() + " §eDistance: §7" + (int)position.getLocation().distance(player.getLocation())));
+
+			} else {
+
+				if (!Challenges.timerIsStarted()) {
+					sender.sendMessage(Prefix.POSITIONS + Translation.TIMER_NOT_STARTED.get());
+					return true;
+				}
+
+				String displayName = args[0];
+
+				position = new ChallengePosition(player.getLocation(), displayName, player.getName());
+				positions.add(position);
+
+				PositionUtil.savePosition(getConfig().toFileConfig(), position);
+				this.getConfig().save();
+
+				String positionName = "§e" + displayName + " §8(" + position.toString() + "§8)";
+				Bukkit.broadcastMessage(Prefix.POSITIONS + Translation.POSITION_CREATED.get().replace("%player%", player.getName()).replace("%position%", positionName));
+
+			}
+
+		} else {
+			sender.sendMessage(Prefix.POSITIONS + LanguageManager.syntax("/position [name]"));
 		}
 
 		return true;
+
 	}
 
 	@Override
-	public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+	public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
 
-		if (args.length != 1) return null;
+		if (args.length != 1) return new ArrayList<>();
 
-		List<String> list = getLocationNamesLowerCase();
-
-		if (!args[0].isEmpty()) {
-			List<String> remove = new ArrayList<>();
-			for (String currentString : list) {
-				if (!currentString.toLowerCase().startsWith(args[0].toLowerCase())) remove.add(currentString);
-			}
-			list.removeAll(remove);
-		}
-
-		return list;
+		ArrayList<String> list = getLocationNames(true);
+		return Utils.getMatchingSuggestions(args[0].toLowerCase(), list);
 
 	}
 
-	private ChallengePosition getPositionByLowerCaseName(String name) {
+	private ChallengePosition getPositionByName(String name) {
 
 		for (ChallengePosition currentPosition : positions) {
 			if (currentPosition == null || currentPosition.getName() == null) continue;
-			if (currentPosition.getName().toLowerCase().equalsIgnoreCase(name)) return currentPosition;
+			if (currentPosition.getName().equalsIgnoreCase(name)) return currentPosition;
 		}
 
 		return null;
 
 	}
 
-	private List<String> getLocationNamesLowerCase() {
+	private ArrayList<String> getLocationNames(boolean lowerCase) {
 
-		List<String> list = new ArrayList<>();
+		ArrayList<String> list = new ArrayList<>();
 
 		for (ChallengePosition currentPosition : positions) {
 			if (currentPosition == null || currentPosition.getName() == null) continue;
-			list.add(currentPosition.getName().toLowerCase());
-		}
-
-		return list;
-
-	}
-
-	private List<String> getLocationNames() {
-
-		List<String> list = new ArrayList<>();
-
-		for (ChallengePosition currentPosition : positions) {
-			list.add(currentPosition.getName());
+			list.add(lowerCase ? currentPosition.getName().toLowerCase() : currentPosition.getName());
 		}
 
 		return list;
