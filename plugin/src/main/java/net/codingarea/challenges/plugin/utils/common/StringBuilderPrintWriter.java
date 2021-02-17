bc7b84d2a80fbe129ad3745bc4b7f020a45c1006
anweisen <anweisen@web.de>
2021-02-17 22:04:12 +0100

    Created utils.common

---
+++ plugin/src/main/java/net/codingarea/challenges/plugin/utils/common/StringBuilderPrintWriter.java
@@ -0,0 +1,27 @@
+package net.codingarea.challenges.plugin.utils.common;
+
+import java.io.PrintWriter;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.3
+ */
+public class StringBuilderPrintWriter extends PrintWriter {
+
+	protected final StringBuilderWriter writer;
+
+	public StringBuilderPrintWriter() {
+		super(new StringBuilderWriter());
+		writer = (StringBuilderWriter) out;
+	}
+
+	public StringBuilder getBuilder() {
+		return writer.getBuilder();
+	}
+
+	@Override
+	public String toString() {
+		return getBuilder().toString();
+	}
+
+}
