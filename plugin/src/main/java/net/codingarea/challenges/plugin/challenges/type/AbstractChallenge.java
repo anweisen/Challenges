ed877dcab505e640fc8bddd95a5530514f7f879b
anweisen <anweisen@web.de>
2021-02-24 10:23:43 +0100

    Added method defaults

--- plugin/src/main/java/net/codingarea/challenges/plugin/challenges/type/AbstractChallenge.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/challenges/type/AbstractChallenge.java
@@ -2,6 +2,8 @@ package net.codingarea.challenges.plugin.challenges.type;
 
 import net.codingarea.challenges.plugin.challenges.type.helper.ChallengeHelper;
 import net.codingarea.challenges.plugin.management.menu.MenuType;
+import net.codingarea.challenges.plugin.utils.config.Document;
+import org.bukkit.event.Listener;
 
 import javax.annotation.Nonnull;
 
@@ -9,7 +11,7 @@ import javax.annotation.Nonnull;
  * @author anweisen | https://github.com/anweisen
  * @since 2.0
  */
-public abstract class AbstractChallenge implements IChallenge {
+public abstract class AbstractChallenge implements IChallenge, Listener {
 
 	protected final MenuType menu;
 
