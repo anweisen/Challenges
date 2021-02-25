768c2b7db4395724f77163fd477e492766cd6c03
anweisen <anweisen@web.de>
2021-02-25 20:02:51 +0100

    Created module MongoConnector as depedency plugin

---
+++ mongo-connector/src/main/java/net/codingarea/challenges/mongoconnector/MongoConnector.java
@@ -0,0 +1,10 @@
+package net.codingarea.challenges.mongoconnector;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 2.0
+ */
+public final class MongoConnector extends JavaPlugin {
+}
