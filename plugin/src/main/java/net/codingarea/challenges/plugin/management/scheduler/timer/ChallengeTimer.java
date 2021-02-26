971733fb75d64c97fd4e32840f8d14839eb08376
anweisen <anweisen@web.de>
2021-02-26 22:13:41 +0100

    Added timer system

---
+++ plugin/src/main/java/net/codingarea/challenges/plugin/management/scheduler/timer/ChallengeTimer.java
@@ -0,0 +1,164 @@
+package net.codingarea.challenges.plugin.management.scheduler.timer;
+
+import net.codingarea.challenges.plugin.Challenges;
+import net.codingarea.challenges.plugin.challenges.type.Goal;
+import net.codingarea.challenges.plugin.management.scheduler.Scheduled;
+import net.codingarea.challenges.plugin.management.scheduler.Scheduled.TimerPolicy;
+import net.codingarea.challenges.plugin.management.server.ChallengeEndCause;
+import net.codingarea.challenges.plugin.utils.animation.SoundSample;
+import net.codingarea.challenges.plugin.utils.config.Document;
+import net.codingarea.challenges.plugin.utils.config.document.YamlDocument;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public final class ChallengeTimer {
+
+	private long time = 0;
+	private boolean countingUp = true;
+	private boolean paused = true;
+
+	private final TimerFormat format;
+	private final String stoppedMessage, upMessage, downMessage;
+
+	public ChallengeTimer() {
+
+		// Load format + messages
+		Document pluginConfig = new YamlDocument(Challenges.getInstance().getConfig());
+
+		Document timerConfig = pluginConfig.getDocument("timer");
+		stoppedMessage = timerConfig.getString("stopped-message", "");
+		upMessage = timerConfig.getString("count-up-message", "");
+		downMessage = timerConfig.getString("count-down-message", "");
+
+		Document formatConfig = timerConfig.getDocument("format");
+		format = new TimerFormat(formatConfig);
+
+		Challenges.getInstance().getScheduler().register(this);
+	}
+
+	@Scheduled(ticks = 20, async = false, timerPolicy = TimerPolicy.ALWAYS)
+	public void onTimerSecond() {
+
+		if (!paused) {
+			if (countingUp) time++;
+			else time--;
+
+			if (time <= 0) {
+				handleHitZero();
+			}
+		}
+
+		updateActionbar();
+		Challenges.getInstance().getMenuManager().updateTimerMenu();
+
+	}
+
+	@Scheduled(ticks = 20, timerPolicy = TimerPolicy.PAUSED)
+	public void onTimerPaused() {
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			if (player.getGameMode() == GameMode.SPECTATOR) continue;
+			Location location = player.getLocation();
+
+			if (location.getWorld() == null) continue;
+			location.getWorld().playEffect(location, Effect.ENDER_SIGNAL, 1);
+		}
+	}
+
+	private void handleHitZero() {
+		Challenges.getInstance().getServerManager().endChallenge(ChallengeEndCause.TIMER_HIT_ZERO);
+	}
+
+	public void resume() {
+
+		Challenges.getInstance().getServerManager().setNotFresh();
+		paused = false;
+		updateActionbar();
+		Challenges.getInstance().getMenuManager().updateTimerMenu();
+
+		Goal currentGoal = Challenges.getInstance().getChallengeManager().getCurrentGoal();
+		if (currentGoal != null)
+			currentGoal.getStartSound().broadcast();
+		else SoundSample.DRAGON_BREATH.broadcast();
+
+	}
+
+	public void pause() {
+		paused = true;
+		updateActionbar();
+		Challenges.getInstance().getMenuManager().updateTimerMenu();
+	}
+
+	public void reset() {
+		time = 0;
+		countingUp = true;
+		updateActionbar();
+		Challenges.getInstance().getMenuManager().updateTimerMenu();
+	}
+
+	public void updateActionbar() {
+		String actionbar = getActionbar();
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(actionbar));
+		}
+	}
+
+	@Nonnull
+	private String getActionbar() {
+		String message = paused ? stoppedMessage : (countingUp ? upMessage : downMessage);
+		String time = getFormattedTime();
+		return message.replace("{time}", time);
+	}
+
+	public void addSeconds(int amount) {
+		time += amount;
+		if (time < 0)
+			time = 0;
+	}
+
+	public void setSeconds(int seconds) {
+		this.time = seconds;
+	}
+
+	public void setCountingUp(boolean countingUp) {
+		this.countingUp = countingUp;
+		updateActionbar();
+		Challenges.getInstance().getMenuManager().updateTimerMenu();
+	}
+
+	@Nonnull
+	public String getFormattedTime() {
+		return format.format(time);
+	}
+
+	@Nonnull
+	public TimerFormat getFormat() {
+		return format;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public boolean isPaused() {
+		return paused;
+	}
+
+	public boolean isStarted() {
+		return !paused;
+	}
+
+	public boolean isCountingUp() {
+		return countingUp;
+	}
+}
---
+++ plugin/src/main/java/net/codingarea/challenges/plugin/management/scheduler/timer/TimerFormat.java
@@ -0,0 +1,76 @@
+package net.codingarea.challenges.plugin.management.scheduler.timer;
+
+import net.codingarea.challenges.plugin.utils.config.Document;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import java.util.Timer;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 2.0
+ */
+public final class TimerFormat {
+
+	private final String seconds, minutes, hours, day, days;
+
+	public TimerFormat(@Nonnull Document document) {
+		 seconds = document.getString("seconds", "");
+		 minutes = document.getString("minutes", "");
+		 hours   = document.getString("hours", "");
+		 day     = document.getString("day", "");
+		 days    = document.getString("days", "");
+	}
+
+	public TimerFormat() {
+		seconds = "{mm}:{ss}";
+		minutes = "{mm}:{ss}";
+		hours   = "{hh}:{mm}:{ss}";
+		day     = "{d}:{hh}:{mm}:{ss}";
+		days    = "{d}:{hh}:{mm}:{ss}";
+	}
+
+	@Nonnull
+	public String format(@Nonnegative long time) {
+
+		long seconds = time;
+		long minutes = seconds / 60;
+		long hours = minutes / 60;
+		long days = hours / 24;
+
+		seconds %= 60;
+		minutes %= 60;
+		hours %= 24;
+
+		return getFormat(minutes, hours, days)
+				.replace("{d}", "" + days)
+				.replace("{dd}", digit2(days))
+				.replace("{h}", "" + hours)
+				.replace("{hh}", digit2(hours))
+				.replace("{m}", "" + minutes)
+				.replace("{mm}", digit2(minutes))
+				.replace("{s}", "" + seconds)
+				.replace("{ss}", digit2(seconds));
+	}
+
+	private String getFormat(long minutes, long hours, long days) {
+		String format;
+		if (days > 1) {
+			format = this.days;
+		} else if (days == 1) {
+			format = this.day;
+		} else if (hours >= 1) {
+			format = this.hours;
+		} else if (minutes >= 1) {
+			format = this.minutes;
+		} else {
+			format = this.seconds;
+		}
+		return format;
+	}
+
+	private String digit2(@Nonnegative long number) {
+		return number > 9 ? "" + number : "0" + number;
+	}
+
+}
