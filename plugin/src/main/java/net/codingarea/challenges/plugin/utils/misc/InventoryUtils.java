84bff8f9e4eaaca15de3ee88e5194b635944fae6
anweisen <anweisen@web.de>
2021-03-03 18:16:43 +0100

    Added more utils to InventoryUtils

--- plugin/src/main/java/net/codingarea/challenges/plugin/utils/misc/InventoryUtils.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/utils/misc/InventoryUtils.java
@@ -24,6 +24,12 @@ public final class InventoryUtils {
 		}
 	}
 
+	public static void fillInventory(@Nonnull Inventory inventory, @Nullable ItemStack item, @Nonnull int... slots) {
+		for (int i : slots) {
+			inventory.setItem(i, item);
+		}
+	}
+
 	public static void close(@Nonnull Inventory inventory) {
 		for (HumanEntity viewer : inventory.getViewers().toArray(new HumanEntity[0])) {
 			viewer.closeInventory();
@@ -43,11 +49,30 @@ public final class InventoryUtils {
 	public static void setNavigationItems(@Nonnull List<Inventory> inventories, @Nonnull int[] navigationSlots, boolean goBackExit) {
 		for (int i = 0; i < inventories.size(); i++) {
 			Inventory inventory = inventories.get(i);
-			ItemStack left = i == 0 && goBackExit ? DefaultItem.navigateBackMainMenu() : DefaultItem.navigateBack();
+			ItemStack left = i == 0 && goBackExit ? DefaultItem.navigateBackMainMenu().build() : DefaultItem.navigateBack().build();
 			inventory.setItem(navigationSlots[0], left);
 			if (i < (inventories.size() - 1))
-				inventory.setItem(navigationSlots[1], DefaultItem.navigateNext());
+				inventory.setItem(navigationSlots[1], DefaultItem.navigateNext().build());
 		}
+	}
+
+	public static boolean isEmpty(@Nonnull Inventory inventory) {
+		for (ItemStack content : inventory.getContents()) {
+			if (content != null) return false;
+		}
+		return true;
+	}
+
+	public static boolean inventoryContainsSequence(@Nonnull Inventory inventory, @Nonnull ItemStack[] sequence) {
+		for (int i = 0; i < sequence.length && i < inventory.getSize(); i++) {
+			ItemStack expected = sequence[i];
+			ItemStack found = inventory.getItem(i);
+			if (expected == null && found == null)  continue;
+			if (expected == null)                   return false;
+			if (found == null)                      return false;
+			if (!expected.isSimilar(found))         return false;
+		}
+		return true;
 	}
 
 }
