bef715b2ac13dc92df3d45a79ea384a9e88cc9b1
anweisen <anweisen@web.de>
2021-03-07 20:00:57 +0100

    Added class comments

--- plugin/src/main/java/net/codingarea/challenges/plugin/management/challenges/ChallengeLoader.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/management/challenges/ChallengeLoader.java
@@ -7,7 +7,11 @@ import net.codingarea.challenges.plugin.challenges.type.IChallenge;
 import javax.annotation.Nonnull;
 
 /**
+ * This class loads all challenges of this plugin.
+ * In pre2 versions, this class was known as PluginChallengeLoader.
+ *
  * @author anweisen | https://github.com/anweisen
+ * @author KxmischesDomi | https://github.com/kxmischesdomi
  * @since 2.0
  */
 public final class ChallengeLoader {
