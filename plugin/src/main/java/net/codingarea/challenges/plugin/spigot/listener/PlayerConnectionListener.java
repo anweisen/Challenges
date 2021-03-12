3d8ec73aa1716579c4e15567f4281f5abbdeb09e
anweisen <anweisen@web.de>
2021-03-12 22:39:53 +0100

    Implemented join & quit messages

--- plugin/src/main/java/net/codingarea/challenges/plugin/spigot/listener/PlayerConnectionListener.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/spigot/listener/PlayerConnectionListener.java
@@ -1,9 +1,11 @@
 package net.codingarea.challenges.plugin.spigot.listener;
 
+import net.codingarea.challenges.plugin.ChallengeAPI;
 import net.codingarea.challenges.plugin.Challenges;
 import net.codingarea.challenges.plugin.lang.Message;
 import net.codingarea.challenges.plugin.lang.Prefix;
 import net.codingarea.challenges.plugin.lang.loader.UpdateLoader;
+import net.codingarea.challenges.plugin.utils.misc.NameHelper;
 import net.codingarea.challenges.plugin.utils.misc.ParticleUtils;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
@@ -21,6 +23,8 @@ import javax.annotation.Nonnull;
  */
 public class PlayerConnectionListener implements Listener {
 
+	private final boolean messages;
+		messages = Challenges.getInstance().getConfigDocument().getBoolean("join-quit-messages");
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onJoin(@Nonnull PlayerJoinEvent event) {
 
@@ -29,6 +33,11 @@ public class PlayerConnectionListener implements Listener {
 		player.getLocation().getChunk().load(true);
 		ParticleUtils.spawnUpGoingParticleCircle(Challenges.getInstance(), player.getLocation(), Particle.SPELL_MOB, 17, 1, 2);
 		Challenges.getInstance().getScoreboardManager().handleJoin(player);
+
+		if (messages) {
+			event.setJoinMessage(null);
+			Message.JOIN_MESSAGE.broadcast(Prefix.CHALLENGES, NameHelper.getName(event.getPlayer()));
+		}
 
 		if (player.hasPermission("challenges.gui")) {
 			if (!UpdateLoader.isNewestPluginVersion()) {
@@ -46,6 +55,13 @@ public class PlayerConnectionListener implements Listener {
 
 		Player player = event.getPlayer();
 		Challenges.getInstance().getScoreboardManager().handleQuit(player);
+
+		if (Challenges.getInstance().getWorldManager().isShutdownBecauseOfReset()) {
+			event.setQuitMessage(null);
+		} else if (messages) {
+			event.setQuitMessage(null);
+			Message.QUIT_MESSAGE.broadcast(Prefix.CHALLENGES, NameHelper.getName(event.getPlayer()));
+		}
 
 	}
 
