d64e16156e05e93c3836eb84fee5376a65d1393d
anweisen <anweisen@web.de>
2021-03-17 10:46:24 +0100

    Implemented message & improved identification

--- plugin/src/main/java/net/codingarea/challenges/plugin/spigot/listener/CheatListener.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/spigot/listener/CheatListener.java
@@ -1,9 +1,13 @@
 package net.codingarea.challenges.plugin.spigot.listener;
 
 import net.codingarea.challenges.plugin.Challenges;
+import net.codingarea.challenges.plugin.lang.Message;
+import net.codingarea.challenges.plugin.lang.Prefix;
 import net.codingarea.challenges.plugin.utils.logging.Logger;
+import net.codingarea.challenges.plugin.utils.misc.NameHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -21,13 +25,13 @@ public class CheatListener implements Listener {
 	public CheatListener() {
 		Bukkit.getOnlinePlayers().stream()
 				.filter(player -> player.getGameMode() == GameMode.CREATIVE)
-				.findFirst().ifPresent(player -> handleCheatsDetected());
+				.findFirst().ifPresent(player -> handleCheatsDetected(player));
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onGameModeChange(@Nonnull PlayerGameModeChangeEvent event) {
 		if (event.getNewGameMode() == GameMode.CREATIVE)
-			handleCheatsDetected();
+			handleCheatsDetected(event.getPlayer());
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
@@ -43,19 +47,42 @@ public class CheatListener implements Listener {
 				"kill"
 		};
 		String message = event.getMessage().toLowerCase();
+		if (message.isEmpty()) return;
+
+		String[] args = message.substring(1).trim().split(" ");
+		String commandName = args[0];
+		if (commandName.contains(":")) {
+			commandName = commandName.substring(commandName.indexOf(':') + 1);
+		}
+
 		for (String command : commands) {
-			if (message.startsWith("/" + command)) {
-				handleCheatsDetected();
-				return;
-			}
+			if (!message.startsWith("/" + command)) continue;
+			if (!hasPermission(event.getPlayer(), command)) continue;
+			handleCheatsDetected(event.getPlayer());
+			break;
 		}
 	}
 
-	private void handleCheatsDetected() {
+	private boolean hasPermission(@Nonnull Player player, @Nonnull String command) {
+		String[] prefixes = {
+			"challenges.",
+			"minecraft.command",
+			"essentials."
+		};
+
+		for (String prefix : prefixes) {
+			if (player.hasPermission(prefix + command))
+				return true;
+		}
+
+		return false;
+	}
+
+	private void handleCheatsDetected(@Nonnull Player player) {
 		if (Challenges.getInstance().getServerManager().hasCheated()) return;
 		Challenges.getInstance().getServerManager().setHasCheated();
 		Logger.info("Detected cheating: No more stats can be collected");
-		// TODO: Chat message
+		Message.forName("cheats-detected").broadcast(Prefix.CHALLENGES, NameHelper.getName(player));
 	}
 
 }
