064b02af8a02d574755fa6972888a11fb5b85d83
anweisen <anweisen@web.de>
2021-03-17 10:47:43 +0100

    Added scoreboard feature to ScoreboardManager

--- plugin/src/main/java/net/codingarea/challenges/plugin/management/server/ScoreboardManager.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/management/server/ScoreboardManager.java
@@ -1,10 +1,13 @@
 package net.codingarea.challenges.plugin.management.server;
 
+import net.codingarea.challenges.plugin.Challenges;
 import net.codingarea.challenges.plugin.management.server.scoreboard.ChallengeBossBar;
+import net.codingarea.challenges.plugin.management.server.scoreboard.ChallengeScoreboard;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,16 +18,25 @@ import java.util.List;
 public final class ScoreboardManager {
 
 	private final List<ChallengeBossBar> bossbars = new ArrayList<>();
+	private ChallengeScoreboard currentScoreboard;
 
 	public void handleQuit(@Nonnull Player player) {
 		for (ChallengeBossBar bossbar : bossbars) {
 			bossbar.applyHide(player);
+		}
+		if (currentScoreboard != null) {
+			currentScoreboard.applyHide(player);
+
+			Bukkit.getScheduler().runTaskLaterAsynchronously(Challenges.getInstance(), () -> currentScoreboard.update(), 1);
 		}
 	}
 
 	public void handleJoin(@Nonnull Player player) {
 		for (ChallengeBossBar bossbar : bossbars) {
 			bossbar.applyShow(player);
+		}
+		if (currentScoreboard != null) {
+			currentScoreboard.applyShow(player);
 		}
 	}
 
@@ -39,10 +51,35 @@ public final class ScoreboardManager {
 		Bukkit.getOnlinePlayers().forEach(bossbar::applyHide);
 	}
 
+	public void setCurrentScoreboard(@Nullable ChallengeScoreboard scoreboard) {
+		if (currentScoreboard == scoreboard) return;
+
+		// Remove old scoreboard
+		if (currentScoreboard != null)
+			Bukkit.getOnlinePlayers().forEach(currentScoreboard::applyHide);
+
+		currentScoreboard = scoreboard;
+
+		// Add new scoreboard if available
+		if (scoreboard == null) return;
+		Bukkit.getOnlinePlayers().forEach(scoreboard::applyShow);
+	}
+
+	@Nullable
+	public ChallengeScoreboard getCurrentScoreboard() {
+		return currentScoreboard;
+	}
+
 	public void disable() {
 		for (ChallengeBossBar bossbar : bossbars.toArray(new ChallengeBossBar[0])) {
 			hideBossBar(bossbar);
 		}
+		setCurrentScoreboard(null);
+	}
+
+	public void handleLoadLanguages() {
+		if (currentScoreboard != null)
+			currentScoreboard.update();
 	}
 
 }
---
+++ plugin/src/main/java/net/codingarea/challenges/plugin/management/server/scoreboard/ChallengeScoreboard.java
@@ -0,0 +1,143 @@
+package net.codingarea.challenges.plugin.management.server.scoreboard;
+
+import net.codingarea.challenges.plugin.Challenges;
+import net.codingarea.challenges.plugin.lang.Message;
+import net.codingarea.challenges.plugin.utils.logging.Logger;
+import net.codingarea.challenges.plugin.utils.misc.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiConsumer;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 2.0
+ */
+public final class ChallengeScoreboard {
+
+	private static int index = 0;
+
+	public final class ScoreboardInstance {
+
+		private final String[] lines = new String[15];
+		private String title = Message.forName("scoreboard-title").asString();
+		private int linesIndex = 0;
+
+		private ScoreboardInstance() {
+		}
+
+		@Nonnull
+		public ScoreboardInstance setTitle(@Nonnull String title) {
+			this.title = title;
+			return this;
+		}
+
+		@Nonnull
+		public ScoreboardInstance addLine(@Nonnull String text) {
+			lines[linesIndex++] = text;
+			return this;
+		}
+
+		@Nonnull
+		public Collection<String> getLines() {
+			List<String> list = new ArrayList<>();
+			for (String line : lines) {
+				if (line == null) continue;
+				list.add(line);
+			}
+			return list;
+		}
+
+		@Nullable
+		public String getTitle() {
+			return title;
+		}
+
+		@Override
+		public String toString() {
+			return "ScoreboardInstance{" +
+					"lines=" + Arrays.toString(lines) +
+					", title='" + title + '\'' +
+					'}';
+		}
+	}
+
+	private final Map<Player, Objective> objectives = new ConcurrentHashMap<>();
+	private BiConsumer<ScoreboardInstance, Player> content = (scoreboard, player) -> {};
+
+	public void setContent(@Nonnull BiConsumer<ScoreboardInstance, Player> content) {
+		this.content = content;
+	}
+
+	public void applyShow(@Nonnull Player player) {
+		update(player);
+	}
+
+	public void applyHide(@Nonnull Player player) {
+		unregister(objectives.get(player));
+	}
+
+	public void update() {
+		Bukkit.getOnlinePlayers().forEach(this::update);
+	}
+
+	public void update(@Nonnull Player player) {
+		try {
+
+			ScoreboardInstance instance = new ScoreboardInstance();
+			content.accept(instance, player);
+
+			Collection<String> lines = instance.getLines();
+			if (lines.isEmpty()) {
+				unregister(objectives.get(player));
+				return;
+			}
+
+			Scoreboard scoreboard = player.getScoreboard();
+			if (scoreboard == Bukkit.getScoreboardManager().getMainScoreboard())
+				player.setScoreboard(scoreboard = Bukkit.getScoreboardManager().getNewScoreboard());
+
+			Objective objective = scoreboard.registerNewObjective(String.valueOf(index++), "dummy", String.valueOf(instance.getTitle()));
+			{
+				int score = lines.size();
+				for (String line : lines) {
+					if (line.isEmpty()) line = StringUtils.repeat(' ', score + 1);
+					score--;
+					objective.getScore(line).setScore(score);
+				}
+			}
+
+			objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+			unregister(objectives.get(player));
+			objectives.put(player, objective);
+
+		} catch (Exception ex) {
+			Logger.severe("Unable to update scoreboard for player '" + player.getName() + "': " + ex.getClass().getSimpleName() + ": " +  ex.getMessage());
+		}
+	}
+
+	public final void show() {
+		Challenges.getInstance().getScoreboardManager().setCurrentScoreboard(this);
+	}
+
+	public final void hide() {
+		if (Challenges.getInstance().getScoreboardManager().getCurrentScoreboard() != this) return;
+		Challenges.getInstance().getScoreboardManager().setCurrentScoreboard(null);
+	}
+
+	private void unregister(@Nullable Objective objective) {
+		try {
+			if (objective == null) return;
+			objective.unregister();
+		} catch (Exception ex) {
+		}
+	}
+
+}
