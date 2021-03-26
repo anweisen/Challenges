ce85f4e23614d9d06aece547ca3ba13ca809aa67
anweisen <anweisen@web.de>
2021-03-27 00:38:13 +0100

    Added a description for setEnabled

--- plugin/src/main/java/net/codingarea/challenges/plugin/challenges/type/Goal.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/challenges/type/Goal.java
@@ -1,5 +1,7 @@
 package net.codingarea.challenges.plugin.challenges.type;
 
+import net.codingarea.challenges.plugin.challenges.type.helper.GoalHelper;
+import net.codingarea.challenges.plugin.management.challenges.ChallengeManager;
 import net.codingarea.challenges.plugin.utils.animation.SoundSample;
 import org.bukkit.entity.Player;
 
@@ -12,6 +14,13 @@ import java.util.List;
  */
 public interface Goal extends IChallenge {
 
+	/**
+	 * Enabled / disabled this goal.
+	 * This must to call {@link ChallengeManager#setCurrentGoal(Goal)} with
+	 * {@code this} if {@code enabled} is {@code true} or {@code null} if {@code enabled} is {@code false} and this goal is the current.
+	 * This can be checked by comparing {@link ChallengeManager#getCurrentGoal()} to {@code this}.
+	 * You may just call {@link GoalHelper#handleSetEnabled(Goal, boolean)} with {@code this}, {@code enabled}
+	 */
 	void setEnabled(boolean enabled);
 
 	@Nonnull
