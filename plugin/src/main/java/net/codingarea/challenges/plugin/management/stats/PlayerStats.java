24276433443515f48c6c4a882f16ba6a3a27fb9a
anweisen <anweisen@web.de>
2021-04-03 21:37:57 +0200

    Include playername in debug

--- plugin/src/main/java/net/codingarea/challenges/plugin/management/stats/PlayerStats.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/management/stats/PlayerStats.java
@@ -34,7 +34,7 @@ public final class PlayerStats {
 	}
 
 	public void incrementStatistic(@Nonnull Statistic statistic, double amount) {
-		Logger.debug("Incrementing statistic {} by {}", statistic, amount);
+		Logger.debug("Incrementing statistic {} by {} for {}", statistic, amount, name);
 		double value = values.getOrDefault(statistic, 0d);
 		values.put(statistic, value + amount);
 	}
