3642f0ee47a4e258a6c26e883d5ee160fd2a1ed7
anweisen <anweisen@web.de>
2021-04-09 18:36:19 +0200

    Add drop-items-on-end setting

--- plugin/src/main/java/net/codingarea/challenges/plugin/management/server/ServerManager.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/management/server/ServerManager.java
@@ -6,13 +6,17 @@ import net.codingarea.challenges.plugin.Challenges;
 import net.codingarea.challenges.plugin.challenges.type.Goal;
 import net.codingarea.challenges.plugin.language.Prefix;
 import net.codingarea.challenges.plugin.utils.animation.SoundSample;
+import net.codingarea.challenges.plugin.utils.bukkit.wrapper.BukkitReflectionUtils;
 import net.codingarea.challenges.plugin.utils.logging.Logger;
 import net.codingarea.challenges.plugin.utils.misc.NameHelper;
 import net.codingarea.challenges.plugin.utils.misc.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.memory.MemoryKey;
+import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
 import java.util.LinkedList;
@@ -28,6 +32,7 @@ public final class ServerManager {
 	private static boolean hasCheated = false;
 
 	private final boolean setSpectatorOnWin;
+	private final boolean dropItemsOnEnd;
 	private final boolean winSounds;
 
 	public ServerManager() {
@@ -37,6 +42,7 @@ public final class ServerManager {
 
 		Document pluginConfig = Challenges.getInstance().getConfigDocument();
 		setSpectatorOnWin = pluginConfig.getBoolean("set-spectator-on-win");
+		dropItemsOnEnd = pluginConfig.getBoolean("drop-items-on-end");
 		winSounds = pluginConfig.getBoolean("enabled-win-sounds");
 	}
 
@@ -64,17 +70,10 @@ public final class ServerManager {
 			return;
 		}
 
-		Challenges.getInstance().getChallengeTimer().pause(false);
-
 		Goal currentGoal = Challenges.getInstance().getChallengeManager().getCurrentGoal();
 		List<Player> winners = new LinkedList<>();
-		if (currentGoal != null)
+		if (currentGoal != null && endCause.isWinnable())
 			currentGoal.getWinnersOnEnd(winners);
-
-		String winnerString = StringUtils.getIterableAsString(winners, ", ", NameHelper::getName);
-		String time = Challenges.getInstance().getChallengeTimer().getFormattedTime();
-		String seed = Bukkit.getWorlds().isEmpty() ? "?" : Bukkit.getWorlds().get(0).getSeed() + "";
-		endCause.getMessage(!winners.isEmpty()).broadcast(Prefix.CHALLENGES, time, winnerString, seed);
 
 		if (endCause != ChallengeEndCause.GOAL_REACHED || setSpectatorOnWin) {
 			setSpectator();
@@ -83,6 +82,20 @@ public final class ServerManager {
 			if (currentGoal.getWinSound() != null)
 				currentGoal.getWinSound().broadcast();
 		}
+		if (dropItemsOnEnd) {
+			for (Player player : Bukkit.getOnlinePlayers()) {
+				if (winners.isEmpty() || winners.contains(player)) continue;
+				dropItems(player);
+			}
+		}
+
+		Challenges.getInstance().getChallengeTimer().pause(false);
+
+		String winnerString = StringUtils.getIterableAsString(winners, ", ", NameHelper::getName);
+		String time = Challenges.getInstance().getChallengeTimer().getFormattedTime();
+		String seed = Bukkit.getWorlds().isEmpty() ? "?" : Bukkit.getWorlds().get(0).getSeed() + "";
+		endCause.getMessage(!winners.isEmpty()).broadcast(Prefix.CHALLENGES, time, winnerString, seed);
+
 	}
 
 	private void setSpectator() {
@@ -94,6 +107,23 @@ public final class ServerManager {
 				player.getWorld().spawnEntity(player.getLocation(), EntityType.FIREWORK);
 			} catch (IllegalArgumentException ex) {
 				// We cant spawn fireworks like that in some versions of spigot
+			}
+		}
+	}
+
+	private void dropItems(@Nonnull Player player) {
+		dropItems(player.getLocation(), player.getInventory().getContents());
+		player.getInventory().clear();
+	}
+
+	private void dropItems(@Nonnull Location location, @Nonnull ItemStack[] items) {
+		for (ItemStack item : items) {
+			if (item == null) continue;
+			if (BukkitReflectionUtils.isAir(item.getType())) continue;
+
+			try {
+				location.getWorld().dropItem(location, item);
+			} catch (IllegalArgumentException ex) {
 			}
 		}
 	}
