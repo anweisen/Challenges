5c8449afa6e6fdfcd16a07036904d8c1c8ca655e
KxmischesDomi <kxmischesdomi@gmail.com>
2021-04-30 15:22:48 +0200

    Add finish raid goal

--- plugin/src/main/java/net/codingarea/challenges/plugin/management/challenges/ChallengeLoader.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/management/challenges/ChallengeLoader.java
@@ -140,6 +140,7 @@ public final class ChallengeLoader extends ModuleChallengeLoader {
 		register(CollectMostExpGoal.class);
 		register(FirstOneToDieGoal.class);
 		register(CollectWoodGoal.class);
+		register(FinishRaidGoal.class);
 
 		// Damage Rules
 		registerDamageRule("fire",      Material.LAVA_BUCKET,               DamageCause.FIRE, DamageCause.FIRE_TICK, DamageCause.LAVA);
