eb0495881dcc4a636fa699d5fbf2140b8f37e624
KxmischesDomi <kxmischesdomi@gmail.com>
2021-05-29 14:47:06 +0200

    Add reload messages

--- plugin/src/main/java/net/codingarea/challenges/plugin/spigot/command/ReloadCommand.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/spigot/command/ReloadCommand.java
@@ -1,6 +1,8 @@
 package net.codingarea.challenges.plugin.spigot.command;
 
 import net.codingarea.challenges.plugin.Challenges;
+import net.codingarea.challenges.plugin.language.Message;
+import net.codingarea.challenges.plugin.language.Prefix;
 import net.codingarea.challenges.plugin.utils.bukkit.command.SenderCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
@@ -15,9 +17,16 @@ public class ReloadCommand implements SenderCommand {
 
 	@Override
 	public void onCommand(@Nonnull CommandSender sender, @Nonnull String[] args) throws Exception {
-		Bukkit.getPluginManager().disablePlugin(Challenges.getInstance());
-		Challenges.getInstance().onLoad();
-		Bukkit.getPluginManager().enablePlugin(Challenges.getInstance());
+		Message.forName("reload").send(sender, Prefix.CHALLENGES);
+		try {
+			Bukkit.getPluginManager().disablePlugin(Challenges.getInstance());
+			Challenges.getInstance().onLoad();
+			Bukkit.getPluginManager().enablePlugin(Challenges.getInstance());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			Message.forName("reload-failed").send(sender, Prefix.CHALLENGES);
+		}
+		Message.forName("reload-success").send(sender, Prefix.CHALLENGES);
 	}
 
 }
