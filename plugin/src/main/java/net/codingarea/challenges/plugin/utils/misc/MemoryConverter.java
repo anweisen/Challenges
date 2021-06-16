fc174c61ecb37f767d54fe12cb6ea7f761547eab
KxmischesDomi <kxmischesdomi@gmail.com>
2021-06-16 16:34:05 +0200

    Fix memory convertion

--- plugin/src/main/java/net/codingarea/challenges/plugin/utils/misc/MemoryConverter.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/utils/misc/MemoryConverter.java
@@ -2,12 +2,10 @@ package net.codingarea.challenges.plugin.utils.misc;
 
 public class MemoryConverter {
 
-    public static int getGB(long bytes) {
-        double kb = (double) bytes / 1024;
-        double mb = kb / 1024;
-        double gb = mb / 1024;
+    private static final long GIGABYTE = 1024L * 1024L * 1024L;
 
-        return (int) Math.ceil(gb);
+    public static int getGB(long bytes) {
+        return (int) (bytes / GIGABYTE);
     }
 
 }
