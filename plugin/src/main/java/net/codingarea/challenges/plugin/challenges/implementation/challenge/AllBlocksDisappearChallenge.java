d9b8645a2ccac2e0f0ec13eb81b8592e5037b1eb
KxmischesDomi <kxmischesdomi@gmail.com>
2022-02-10 18:46:01 +0100

    Add drop limit

--- plugin/src/main/java/net/codingarea/challenges/plugin/challenges/implementation/challenge/AllBlocksDisappearChallenge.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/challenges/implementation/challenge/AllBlocksDisappearChallenge.java
@@ -2,8 +2,10 @@ package net.codingarea.challenges.plugin.challenges.implementation.challenge;
 
 import net.anweisen.utilities.bukkit.utils.misc.BukkitReflectionUtils;
 import net.anweisen.utilities.common.annotations.Since;
+import net.anweisen.utilities.common.config.Document;
 import net.codingarea.challenges.plugin.Challenges;
 import net.codingarea.challenges.plugin.challenges.type.abstraction.MenuSetting;
+import net.codingarea.challenges.plugin.challenges.type.helper.ChallengeConfigHelper;
 import net.codingarea.challenges.plugin.challenges.type.helper.ChallengeHelper;
 import net.codingarea.challenges.plugin.content.Message;
 import net.codingarea.challenges.plugin.management.menu.MenuType;
@@ -24,9 +26,7 @@ import org.bukkit.inventory.PlayerInventory;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author KxmischesDomi | https://github.com/kxmischesdomi
@@ -34,6 +34,8 @@ import java.util.List;
  */
 @Since("2.0")
 public class AllBlocksDisappearChallenge extends MenuSetting {
+
+	private final int stackDropLimit;
 
 	public AllBlocksDisappearChallenge() {
 		super(MenuType.CHALLENGES, "All Blocks Disappear");
@@ -45,6 +47,8 @@ public class AllBlocksDisappearChallenge extends MenuSetting {
 				() -> new ItemBuilder(Material.DIAMOND_BLOCK, Message.forName("item-all-blocks-disappear-place-challenge"))
 		));
 
+		Document document = ChallengeConfigHelper.getSettingsDocument();
+		stackDropLimit = document.contains("all-block-disappear-stack-drop-limit") ? document.getInt("all-block-disappear-stack-drop-limit") : 50;
 	}
 
 	@Nonnull
@@ -106,10 +110,22 @@ public class AllBlocksDisappearChallenge extends MenuSetting {
 
 	private void dropList(@Nonnull Collection<ItemStack> itemStacks, @Nonnull Location location, @Nonnull Inventory inventory) {
 		if (location.getWorld() == null) return;
+		Map<Material, Integer> stackCount = new HashMap<>();
+
 		for (ItemStack itemStack : itemStacks) {
-			ChallengeHelper.dropItem(itemStack, location, inventory);
-			location.getWorld().dropItemNaturally(location, itemStack);
+			if (increaseStackCount(stackCount, itemStack.getType())) {
+				ChallengeHelper.dropItem(itemStack, location, inventory);
+				location.getWorld().dropItemNaturally(location, itemStack);
+			}
 		}
+	}
+
+	private boolean increaseStackCount(Map<Material, Integer> map, Material material) {
+		int droppedStacks = map.getOrDefault(material, 0);
+		if (droppedStacks >= stackDropLimit) return false;
+		droppedStacks++;
+		map.put(material, droppedStacks);
+		return true;
 	}
 
 	protected List<Block> getAllBlocksToBreak(@Nonnull Chunk chunk, @Nonnull Material material) {
@@ -117,9 +133,7 @@ public class AllBlocksDisappearChallenge extends MenuSetting {
 				.fill(builder -> {
 					for (int x = 0; x < 16; x++) {
 						for (int z = 0; z < 16; z++) {
-
 							for (int y = Utils.getMinHeight(chunk.getWorld()); y <= chunk.getWorld().getMaxHeight(); y++) {
-								System.out.println(y);
 								Block block = chunk.getBlock(x, y, z);
 								if (block.getType() == material) {
 									builder.add(block);
