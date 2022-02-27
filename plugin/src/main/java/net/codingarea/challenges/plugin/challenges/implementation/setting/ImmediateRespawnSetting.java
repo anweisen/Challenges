8d3bb5898e66ed0115ce8b236e6f1b414ca781f4
KxmischesDomi <kxmischesdomi@gmail.com>
2022-02-27 15:53:10 +0100

    Replace respawn method with gamerules

--- plugin/src/main/java/net/codingarea/challenges/plugin/challenges/implementation/setting/ImmediateRespawnSetting.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/challenges/implementation/setting/ImmediateRespawnSetting.java
@@ -1,17 +1,15 @@
 package net.codingarea.challenges.plugin.challenges.implementation.setting;
 
+import javax.annotation.Nonnull;
 import net.anweisen.utilities.common.annotations.Since;
 import net.codingarea.challenges.plugin.challenges.type.abstraction.Setting;
 import net.codingarea.challenges.plugin.content.Message;
 import net.codingarea.challenges.plugin.management.menu.MenuType;
 import net.codingarea.challenges.plugin.utils.item.ItemBuilder;
 import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
 import org.bukkit.Material;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.entity.PlayerDeathEvent;
-
-import javax.annotation.Nonnull;
+import org.bukkit.World;
 
 /**
  * @author KxmischesDomi | https://github.com/kxmischesdomi
@@ -30,10 +28,18 @@ public class ImmediateRespawnSetting extends Setting {
 		return new ItemBuilder(Material.GOLDEN_APPLE, Message.forName("item-immediate-respawn-setting"));
 	}
 
-	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-	public void onPlayerDeath(@Nonnull PlayerDeathEvent event) {
-		if (!isEnabled()) return;
-		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> event.getEntity().spigot().respawn(), 1);
+	@Override
+	protected void onEnable() {
+		for (World world : Bukkit.getWorlds()) {
+			world.setGameRule(GameRule.DO_IMMEDIATE_RESPAWN, true);
+		}
+	}
+
+	@Override
+	protected void onDisable() {
+		for (World world : Bukkit.getWorlds()) {
+			world.setGameRule(GameRule.DO_IMMEDIATE_RESPAWN, false);
+		}
 	}
 
 }
