4a31ca6b94f33af8480c93ceb9a47402ee946398
KxmischesDomi <kxmischesdomi@gmail.com>
2022-03-02 17:10:49 +0100

    Remove level border for reimplementation later

--- plugin/src/main/java/net/codingarea/challenges/plugin/management/challenges/ChallengeLoader.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/management/challenges/ChallengeLoader.java
@@ -154,7 +154,6 @@ public final class ChallengeLoader extends ModuleChallengeLoader {
 
 		register(UncraftItemsChallenge.class);
 		register(FiveHundredBlocksChallenge.class);
-		register(LevelBorderChallenge.class);
 
 		// Goal
 		register(KillEnderDragonGoal.class);
