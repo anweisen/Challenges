fdd156e42557beeedf4fa417b663712b77a3aa51
KxmischesDomi <kxmischesdomi@gmail.com>
2022-04-04 15:03:01 +0200

    Revert "Don't send any actionbar packets if hidden"
    This reverts commit 84136676f82e5cc3dbccf92980ec1c9da2731b0f.

--- plugin/src/main/java/net/codingarea/challenges/plugin/management/scheduler/timer/ChallengeTimer.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/management/scheduler/timer/ChallengeTimer.java
@@ -153,10 +153,10 @@ public final class ChallengeTimer {
 	public void updateActionbar() {
 		if (sentEmpty && hidden) return;
 		if (hidden) sentEmpty = true;
-		if (!hidden) {
-			for (Player player : Bukkit.getOnlinePlayers()) {
-				player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(getActionbar()));
-			}
+		String actionbar = hidden ? "" : getActionbar();
+
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(actionbar));
 		}
 	}
 
