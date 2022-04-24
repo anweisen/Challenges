36a0bba8cd3413fec73c27eae4799f374d62efe1
KxmischesDomi <kxmischesdomi@gmail.com>
2022-04-24 17:19:47 +0200

    Reformat code

--- plugin/src/main/java/net/codingarea/challenges/plugin/utils/bukkit/container/BukkitSerialization.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/utils/bukkit/container/BukkitSerialization.java
@@ -29,15 +29,14 @@ public class BukkitSerialization {
 		String content = toBase64(playerInventory);
 		String armor = itemStackArrayToBase64(playerInventory.getArmorContents());
 
-		return new String[] { content, armor };
+		return new String[]{content, armor};
 	}
 
 	/**
-	 *
 	 * A method to serialize an {@link ItemStack} array to Base64 String.
-	 *
-	 * <p />
-	 *
+	 * <p>
+	 * <p/>
+	 * <p>
 	 * Based off of {@link #toBase64(Inventory)}.
 	 *
 	 * @param items to turn into a Base64 String.
@@ -67,9 +66,9 @@ public class BukkitSerialization {
 
 	/**
 	 * A method to serialize an inventory to Base64 string.
-	 *
-	 * <p />
-	 *
+	 * <p>
+	 * <p/>
+	 * <p>
 	 * Special thanks to Comphenix in the Bukkit forums or also known
 	 * as aadnk on GitHub.
 	 *
@@ -98,11 +97,10 @@ public class BukkitSerialization {
 	}
 
 	/**
-	 *
 	 * A method to get an {@link Inventory} from an encoded, Base64, string.
-	 *
-	 * <p />
-	 *
+	 * <p>
+	 * <p/>
+	 * <p>
 	 * Special thanks to Comphenix in the Bukkit forums or also known
 	 * as aadnk on GitHub.
 	 *
@@ -131,9 +129,9 @@ public class BukkitSerialization {
 
 	/**
 	 * Gets an array of ItemStacks from Base64 string.
-	 *
-	 * <p />
-	 *
+	 * <p>
+	 * <p/>
+	 * <p>
 	 * Base off of {@link #fromBase64(String)}.
 	 *
 	 * @param data Base64 string to convert to ItemStack array.
