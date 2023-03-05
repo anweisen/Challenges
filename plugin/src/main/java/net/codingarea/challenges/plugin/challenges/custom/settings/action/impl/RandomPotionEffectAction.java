370ca5941f07faf3ba92f0aabb34fbc9c7695153
Sehne, Julian <julian.sehne@student.fhws.de>
2023-03-05 19:28:03 +0100

    Multiply potion duration

--- plugin/src/main/java/net/codingarea/challenges/plugin/challenges/custom/settings/action/impl/RandomPotionEffectAction.java
+++ plugin/src/main/java/net/codingarea/challenges/plugin/challenges/custom/settings/action/impl/RandomPotionEffectAction.java
@@ -1,14 +1,18 @@
 package net.codingarea.challenges.plugin.challenges.custom.settings.action.impl;
-
+import net.anweisen.utilities.bukkit.utils.logging.Logger;
+import net.codingarea.challenges.plugin.ChallengeAPI;
+import net.codingarea.challenges.plugin.Challenges;
 import net.codingarea.challenges.plugin.challenges.custom.settings.action.EntityTargetAction;
 import net.codingarea.challenges.plugin.challenges.implementation.challenge.RandomPotionEffectChallenge;
 import net.codingarea.challenges.plugin.challenges.type.helper.SubSettingsHelper;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,6 +26,7 @@ public class RandomPotionEffectAction extends EntityTargetAction {
 				SubSettingsHelper.createPotionSettingsBuilder(false, true)));
 	}
 
+
 	@Override
 	public void executeFor(Entity entity, Map<String, String[]> subActions) {
 		if (entity instanceof LivingEntity) {
@@ -30,7 +35,19 @@ public class RandomPotionEffectAction extends EntityTargetAction {
 					.getNewRandomEffect(livingEntity);
 
 			if (randomEffect == null) return;
-			PotionEffect effect = randomEffect.createEffect(Integer.parseInt(subActions.get("length")[0]) * 20 + 1,
+			int length = Integer.parseInt(subActions.get("length")[0]);
+			String[] length_multipliers = subActions.get("length_multiplier");
+			int length_multiplier = 1;
+			if(length_multipliers != null){
+				length_multiplier= Integer.parseInt(length_multipliers[0]);
+			}
+
+			List<Player> ingamePlayers =
+				ChallengeAPI.getIngamePlayers();
+
+			Challenges.getInstance().getLogger().debug("length and length_multiplier",length, length_multiplier);
+			Logger.debug("length and length_multiplier",length, length_multiplier);
+			PotionEffect effect = randomEffect.createEffect((length * 20 + 1) * length,
 					Integer.parseInt(subActions.get("amplifier")[0]));
 
 			livingEntity.addPotionEffect(effect);
